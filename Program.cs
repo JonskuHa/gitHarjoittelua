dc99b9763c80d597ba16ae430d322c87dc63d58a
Joona Harjula <joona.harjula@novaservo.fi>
2023-02-22 09:35:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Hello World! program
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
